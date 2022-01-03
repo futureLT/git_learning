8e2afd4f89359c0c2bd9d0e65d4929e64b1fa3be
lt <1987563420@qq.com>
2022-01-03 14:53:04 +0800

    学习通过git进行代码，并修改提交信息

---
+++ javaProject/src/com/lt/ThreadTest.java
@@ -0,0 +1,24 @@
+package com.lt;
+
+/**
+ * 多线程学习
+ * @author lutao
+ * @create 2021/10/31
+ */
+public class ThreadTest {
+    public static void main(String[] args) {
+
+    }
+}
+
+
+/**
+ * 实现Runnable接口
+ */
+class Handler implements Runnable {
+
+    @Override
+    public void run() {
+
+    }
+}
